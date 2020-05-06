aa0757199e54bde659977f0c3789821c8f9bf6de
Uriah Harston <uharston@gmail.com>
2020-05-06 20:40:11 +0000

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,11 +1,14 @@
 require 'pry'
-
+#while loop w/ yield is what happen under an each or collect block
 def hello(array)
   i = 0
+  collection = []
   while i < array.length
-    yield(array[i])
+    collection << yield(array[i])
+    binding.pry
     i += 1
   end
+  collection
 end
 
 
